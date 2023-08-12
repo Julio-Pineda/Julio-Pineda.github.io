87c7f4f2a0a180c4a93aa4fc2cf3784474592a81
Julio Pineda <pjulio@unicauca.edu.co>
2023-08-12 15:37:28 -0500

    Download CV in English-Spanish

--- script/script.js
+++ script/script.js
@@ -81,8 +81,8 @@ document.getElementById('form')
 });
 
 
-  document.getElementById("btnDescargar").addEventListener("click", function() {
-  var fileUrl = "./assets/Julio.pdf";
+function downloadEnglishCv () {
+  var fileUrl = "./assets/Julio_en.pdf";
 
   var fileName = "CV - JULIO CÉSAR PINEDA MORA - WEB DEVELOPER.pdf";
 
@@ -96,6 +96,70 @@ document.getElementById('form')
 
   // Remueve el enlace del DOM después de la descarga
   document.body.removeChild(link);
+}
+
+function downloadSpanishCv () {
+  var fileUrl = "./assets/Julio_es.pdf";
+
+  var fileName = "CV - JULIO CÉSAR PINEDA MORA - DESARROLLADOR WEB.pdf";
+
+  var link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+
+  // Simula un clic en el enlace oculto para descargar el archivo
+  document.body.appendChild(link);
+  link.click();
+
+  // Remueve el enlace del DOM después de la descarga
+  document.body.removeChild(link);
+}
+
+
+document.getElementById("btnDescargar").addEventListener("click", function() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-success',
+    },
+    buttonsStyling: true,
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Descargar / Download',
+    text: "Elija el idioma / Choose the language",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'English',
+    cancelButtonText: 'Español',
+    confirmButtonColor: '#3368FF',
+    cancelButtonColor: '#3368FF',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      downloadEnglishCv ();
+      swalWithBootstrapButtons.fire(
+        'Downloaded',
+        'Curriculum vitae has been downloaded correctly!',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      downloadSpanishCv ();
+      swalWithBootstrapButtons.fire(
+        'Descargado',
+        '¡Curriculum vitae descargado correctamente!',
+        'success'
+      )
+    }
+  })
 });
+
+
+
+
+
 
 
